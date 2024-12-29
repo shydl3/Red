86ec96183cd35b7a08aabb052a14590cffad0aa6
shydl3 <68953109+shydl3@users.noreply.github.com>
2024-12-28 18:59:12 -0800

    Add files via upload

--- shy/数组/209 长度最小子数组.py
+++ shy/数组/209 长度最小子数组.py
@@ -1,3 +1,25 @@
+# 滑动窗口
+class Solution:
+    def minSubArray(self, s, nums):
+        if not nums:
+            return 0
+
+        n = len(nums)
+        ans = n + 1
+        start, end = 0, 0
+        total = 0
+
+        while end < n:
+            total += nums[end]
+            while total > s:
+                ans = min(ans, end - start + 1)
+                total -= nums[start]
+                start += 1
+
+            end += 1
+
+        return 0 if ans == n+1 else ans
+
 
 
 
