82555af22ef6178c55d86409726728c502bbc405
shydl3 <68953109+shydl3@users.noreply.github.com>
2024-12-28 19:53:34 -0800

    Add files via upload

--- shy/数组/704 二分查找.py
+++ shy/数组/704 二分查找.py
@@ -1,3 +1,8 @@
+'''
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+'''
+
 class Solution:
     def search(self, nums, target):
         left, right = 0, len(nums) - 1
