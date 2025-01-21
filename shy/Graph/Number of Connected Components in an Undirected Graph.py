196a53d12c184262c2a3339da6010e61f9062566
shydl3 <68953109+shydl3@users.noreply.github.com>
2025-01-21 02:04:46 -0800

    Add files via upload

---
+++ shy/Graph/Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        stack = [0]
+        seen = {0}
+        ans = 0
+
+        def dfs(node):
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
+        # while stack:
+        #     node = stack.pop()
+        #     for neighbor in graph[node]:
+
+        # for node in range(n):
+        #     for neighbor in graph[node]:
+        #         if neighbor not in seen:
+        #             ans += 1
+
+        for node in range(n):
+            if node not in seen:
+                dfs(node)
+                ans += 1
+        return ans
